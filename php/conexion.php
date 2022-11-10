72a503ef6f7e9f5c3a5d84096868eddd3b097654
sil03perez <2190025@ipisa.edu.do>
2022-11-10 15:18:29 -0400

    redireccion

--- php/conexion.php
+++ php/conexion.php
@@ -1,5 +1,5 @@
 <?php
-$conn_practicaphp = new mysqli("localhost", "root", "", "mfct_bd");
+$conn_practicaphp = new mysqli("localhost", "root", "123456789", "mfct_bd");
 
 if ($conn_practicaphp->connect_error) {
     die("Error: ".$conn_practicaphp->connect_errno ." " .$conn_practicaphp->connect_error);

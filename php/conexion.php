edd4cce3789296da401e5437dea446f0714ef108
sil03perez <2190025@ipisa.edu.do>
2022-11-11 12:58:22 -0400

    conexio

--- php/conexion.php
+++ php/conexion.php
@@ -1,5 +1,5 @@
 <?php
-$conn_practicaphp = new mysqli("localhost", "root", "Erwin123456", "mfct_bd");
+$conn_practicaphp = new mysqli("localhost", "root", "123456789", "mfct_bd");
 
 if ($conn_practicaphp->connect_error) {
     die("Error: ".$conn_practicaphp->connect_errno ." " .$conn_practicaphp->connect_error);

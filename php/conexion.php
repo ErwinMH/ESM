c08f2577f655900014299b93cbffdeb965d9e9e0
sil03perez <2190025@ipisa.edu.do>
2022-11-11 08:09:18 -0400

    commit

--- php/conexion.php
+++
@@ -1,8 +0,0 @@
-<?php
-$conn_practicaphp = new mysqli("localhost", "root", "123456789", "mfct_bd");
-
-if ($conn_practicaphp->connect_error) {
-    die("Error: ".$conn_practicaphp->connect_errno ." " .$conn_practicaphp->connect_error);
-}
-
-?>

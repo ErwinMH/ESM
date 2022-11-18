26f533d52cdd69eb49010e62ef3c87ecc6f90ce4
sil03perez <2190025@ipisa.edu.do>
2022-11-17 23:00:09 -0400

    arreglando

--- php/frm2/edit.php
+++ php/frm2/edit.php
@@ -2,30 +2,30 @@
 include_once("../conexion.php");
 
     $cod = $_REQUEST['id'];
-    $nom_empr =$_REQUEST['nom_empr'];
-        $rnc = $_REQUEST['rnc'];
-        $id_em = $_REQUEST['ident'];
-        $dept = $_REQUEST['dept'];
-        $alcance = $_REQUEST['alcance'];
-        $acti = $_REQUEST['act_eco'];
-        $indu = $_REQUEST['indu'];
-        $tama = $_REQUEST['cap'];
-        $direc = $_REQUEST['dir'];
-        $sector = $_REQUEST['sect'];
-        $secc = $_REQUEST['secc'];
-        $provincia = $_REQUEST['prov'];
-        $municipo = $_REQUEST['mun'];
-        $pais = $_REQUEST['pais'];
-        $telpri = $_REQUEST['tel_prin'];
-        $telsec = $_REQUEST['tel_dir'];
+    $nom_empr =$_REQUEST['nom_empresa'];
+        $rnc = $_REQUEST['RNC'];
+        $id_em = $_REQUEST['identidad_empresa'];
+        $dept = $_REQUEST['departamento_formacion'];
+        $alcance = $_REQUEST['alcance_empresa'];
+        $acti = $_REQUEST['actividad_economica'];
+        $indu = $_REQUEST['industria'];
+        $tama = $_REQUEST['tamaño_empresa'];
+        $direc = $_REQUEST['direccion'];
+        $sector = $_REQUEST['sector'];
+        $secc = $_REQUEST['seccion'];
+        $provincia = $_REQUEST['provincia'];
+        $municipo = $_REQUEST['municipio'];
+        $pais = $_REQUEST['pais_operacion'];
+        $telpri = $_REQUEST['telefono_pri'];
+        $telsec = $_REQUEST['telefono_di'];
         $contacto = $_REQUEST['cont_empr'];
         $mail = $_REQUEST['email'];
-        $clave = $_REQUEST['clave'];
+        $clave = $_REQUEST['contraseña'];
         $tel_ext = $_REQUEST['tel_ext'];
     $update="UPDATE datos_form2 SET nombre_empresa='$nom_empr', RNC='$rnc', identidad_empresa='$id_em', 
     departamento_formacion='$dept', alcance_empresa='$alcance', actividad_economica='$acti',
-     industria='$indu', tamaño_empresa='$tama', direccion='$direc', sector='$sector',seccion='$secc'
-     provincia='$provincia', municipio='$municipio', pais_operacion='$pais', telefono_pri='$telpri'
+     industria='$indu', tamaño_empresa='$tama', direccion='$direc', sector='$sector',seccion='$secc',
+     provincia='$provincia', municipio='$municipio', pais_operacion='$pais', telefono_pri='$telpri',
      telefono_di='$telsec', email='$mail', contraseña='$clave', tel_ext='$tel_ext' WHERE id=$cod";
     
     mysqli_query($conn_practicaphp, $update);
